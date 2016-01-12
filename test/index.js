3d9741dc8210888a421be640229578b5b13c602e
Fedor Baart <fedor.baart@deltares.nl>
2016-01-12 23:03:30 +0100

    es6

--- test/index.js
+++ test/index.js
@@ -1,30 +1,31 @@
 import assert from 'assert';
 import wvs from '../lib';
-import fs from 'fs';
 
-describe('wvs', function () {
-  it('should be able to use the tile-generator function', function (done) {
-    var tiles = [];
+describe('wvs', () => {
+  it('should be able to use the tile-generator function', done => {
+    const tiles = [];
     wvs.tileGenerator(10, [0, 30, 5, 35])
-      .on('data', function (tile) {
+      .on('data', tile => {
         tiles.push(tile);
       })
-      .on('end', function(){
+      .on('end', () => {
         assert(tiles.length > 0, 'we should have tiles as a result.');
         done();
       });
   });
-  it('should be possible to get tile properties', function () {
-    let testTile =  [3,4,6];
-    let children = wvs.tilebelt.getChildren(testTile);
+  it('should be possible to get tile properties', () => {
+    const testTile = [3, 4, 6];
+    const children = wvs.tilebelt.getChildren(testTile);
     assert(children, 'we should be lookup the children.');
   });
-  it('should be able to create an ffmpeg command', function (done) {
-    let testFile = 'examples/sync/app/movies/chicken.webm';
-    wvs.ffmpeg.ffprobe(testFile, function(err, metadata) {
+  it('should be able to create an ffmpeg command', done => {
+    const testFile = 'examples/sync/app/movies/chicken.webm';
+    wvs.ffmpeg.ffprobe(testFile, (err, metadata) => {
+      if (err) {
+        console.log('error in ffprobe', err);
+      }
       assert(metadata.format.filename === testFile, 'we should have a filename');
       done();
     });
-
   });
 });
